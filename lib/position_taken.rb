8c4f8838f728be03296882050fae0c533e659ff3
Jose Melero Martinez <jmelero08@gmail.com>
2020-10-15 19:33:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,8 +2,5 @@
 
 def position_taken?(board, index)
   if board[index] != "" || board[index] != " " || board[index] != nil 
-    return false
-  else board[index] == "X" || board[index] == "O"
-    return true
   end
 end
