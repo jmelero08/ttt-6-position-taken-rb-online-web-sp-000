58acf2d622ef258b7e83ab8f5f1c090324675028
Jose Melero Martinez <jmelero08@gmail.com>
2020-10-15 19:37:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] != "" || board[index] != " " || board[index] != nil 
+  if board[0] != "" || board[0] != " " || board[0] != nil 
     return false
+  end
 end
